7bb45bd8bb6268e3e0ead12e378950cc2d89329d
Vitor Rodrigues <vitor_rc@outlook.com>
2021-06-25 09:48:44 -0300

    Reorganizacao dos arquivos, configs do docker e inicio do front

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const todosRoute = require('./src/routes/todosRoute');
 
 const app = express();
-const PORT = 3001;
+const PORT = 3002;
 
 app.use(express.json());
 app.use(todosRoute);
